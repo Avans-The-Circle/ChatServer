3fc05a9f6648ca3dfd814bd4de6a0973228caed3
onno204 <15333546+onno204@users.noreply.github.com>
2022-06-15 11:22:30 +0200

    Added streamframe

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
+console.log("Running in port", process.env.PORT || 8080)
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
         data = JSON.parse(data);
-        console.log(data);
+        // console.log(data);
         switch (data.type) {
             case "OPEN_CONNECTION":
                 ws.streamId = data.streamId;
                 ws.send(JSON.stringify({ "type": "CONFIRM_CONNECTION", "streamId": data.streamId }));
                 break;
+            case "STREAM_FRAME":
+                wss.clients.forEach(function each(client) {
+                    if (ws.streamId === client.streamId) {
+                        client.send(JSON.stringify({
+                                "type": "INCOMMING_STREAM",
+                                "frame": data.frame,
+                            }
+                        ));
+                    }
+                });
+                break;
             case "SEND_MESSAGE":
-                if(ws.streamId == -1) return;
+                if(ws.streamId === -1) return;
                 //Message to backend
                 wss.clients.forEach(function each(client) {
-                    if (ws.streamId == client.streamId) {
+                    if (ws.streamId === client.streamId) {
                         client.send(JSON.stringify({
                             "type": "CHAT_MESSAGE",
                             "message": data.message,
