9dbb522197da9eb27c8f3ec6b7aee91c0ea8a15a
onno204 <15333546+onno204@users.noreply.github.com>
2022-06-20 14:33:21 +0200

    Fixed FPS more stream

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import { WebSocketServer } from 'ws';
+import lzstring from 'lz-string';
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 console.log("Running in port", process.env.PORT || 8080)
 wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
+    console.log("new ocnnection")
+    ws.on('message', async function message(data) {
         data = JSON.parse(data);
+        if (data.compressed === true) {
+            data = JSON.parse(lzstring.decompress(data.data));
+        }
         // console.log(data);
         switch (data.type) {
             case "OPEN_CONNECTION":
@@ -12,6 +17,11 @@ wss.on('connection', function connection(ws) {
                 ws.send(JSON.stringify({ "type": "CONFIRM_CONNECTION", "streamId": data.streamId }));
                 break;
             case "STREAM_FRAME":
+                console.log(`[${data.frameCounter}]incomming frame ${data.frame_timing} == ${(new Date()).getTime()}`)
+                ws.send(JSON.stringify({
+                        "type": "SEND_NEXT_FRAME"
+                    }
+                ))
                 wss.clients.forEach(function each(client) {
                     if (ws.streamId === client.streamId) {
                         client.send(JSON.stringify({
