bf356e6ecc06b84a952117b9c79fde67c25b61df
onno204 <15333546+onno204@users.noreply.github.com>
2022-06-21 10:14:42 +0200

    Fixed SSL keys

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import { createServer } from 'http';
+import { createServer } from 'https';
 import { parse } from 'url';
 import lzstring from 'lz-string';
 import { WebSocketServer } from 'ws';
+import { readFileSync } from 'fs';
 
-const server = createServer();
-const wss = new WebSocketServer({ noServer: true });
-const wssBinary = new WebSocketServer({ noServer: true });
+const server = createServer({
+    key: readFileSync('./keys/key.pem'),
+    cert: readFileSync('./keys/cert.pem')
+});
+const wss = new WebSocketServer({noServer: true});
+const wssBinary = new WebSocketServer({noServer: true});
 wssBinary.binaryType = "blob";
+
 
 wssBinary.on('connection', function connection(ws) {
     console.log("[binary]Connection started", ws.streamId)
@@ -14,21 +19,21 @@ wssBinary.on('connection', function connection(ws) {
         console.log("[binary]received data", isBinary)
         wss.clients.forEach(function each(client) {
             if (ws.streamId === client.streamId) {
-                client.send(data, { binary: isBinary });
+                client.send(data, {binary: isBinary});
             }
         });
     });
 });
 
 server.on('upgrade', function upgrade(request, socket, head) {
-    const { pathname } = parse(request.url);
+    const {pathname} = parse(request.url);
     if (pathname.split('/')[1] === "binary") {
         console.log("Upgrading to binary stream", pathname)
-        wss.handleUpgrade(request, socket, head, function done(ws) {
+        wssBinary.handleUpgrade(request, socket, head, function done(ws) {
             ws.streamId = pathname.split('/')[2]
-            wss.emit('connection', ws, request);
+            wssBinary.emit('connection', ws, request);
         });
-    }else{
+    } else {
         console.log("Upgrading to websocket")
         wss.handleUpgrade(request, socket, head, function done(ws) {
             wss.emit('connection', ws, request);
@@ -51,7 +56,7 @@ wss.on('connection', function connection(ws) {
         switch (data.type) {
             case "OPEN_CONNECTION":
                 ws.streamId = data.streamId;
-                ws.send(JSON.stringify({ "type": "CONFIRM_CONNECTION", "streamId": data.streamId }));
+                ws.send(JSON.stringify({"type": "CONFIRM_CONNECTION", "streamId": data.streamId}));
                 break;
             case "STREAM_FRAME":
                 console.log(`[${data.frameCounter}]incomming frame ${data.frame_timing} == ${(new Date()).getTime()}`)
@@ -70,15 +75,15 @@ wss.on('connection', function connection(ws) {
                 ))
                 break;
             case "SEND_MESSAGE":
-                if(ws.streamId === -1) return;
+                if (ws.streamId === -1) return;
                 //Message to backend
                 wss.clients.forEach(function each(client) {
                     if (ws.streamId === client.streamId) {
                         client.send(JSON.stringify({
-                            "type": "CHAT_MESSAGE",
-                            "message": data.message,
-                            "sender": data.sender
-                        }
+                                "type": "CHAT_MESSAGE",
+                                "message": data.message,
+                                "sender": data.sender
+                            }
                         ));
                     }
                 });
