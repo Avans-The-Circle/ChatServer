147190b4aa8e1a78f4b9ea36f1b5cec846016891
Luuk Bartels <l.bartes1@student.avans.nl>
2022-06-21 13:49:41 +0200

    added livestream/ node-media-server

---
+++ livestream.js
@@ -0,0 +1,32 @@
+import NodeMediaServer from 'node-media-server'
+
+const config = {
+        rtmp: {
+          port: 1935,
+          chunk_size: 60000,
+          gop_cache: false,
+          ping: 60,
+          ping_timeout: 30,
+        },
+        http: {
+          port: 8000,
+          mediaroot: './media/server',
+          allow_origin: '*',
+        },
+        trans: {
+          ffmpeg: '/usr/bin/ffmpeg',
+          tasks: [
+            {
+              app: 'live',
+              hls: true,
+              hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+              dash: true,
+              dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+            },
+          ],
+        },
+}
+
+
+var nms = new NodeMediaServer(config)
+nms.run();
