c00264b6b8ad19d63d70fe32bb82f8538ccd094c
TimdeLaater <tdelaater@gmail.com>
2022-06-22 14:19:21 +0200

    Fixed android stream

--- index.js
+++ index.js
@@ -2,30 +2,32 @@ import { WebSocketServer } from 'ws';
 import NodeMediaServer from 'node-media-server'
 
 const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: false,
-    ping: 60,
-    ping_timeout: 30,
-  },
-  http: {
-    port: 8000,
-    mediaroot: './media/server',
-    allow_origin: '*',
-  },
-  trans: {
-    ffmpeg: './ffmpeg.exe',
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
-  }
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: false,
+        ping: 60,
+        ping_timeout: 30,
+    },
+    http: {
+        port: 8000,
+        mediaroot: './media/server',
+        allow_origin: '*',
+    },
+    trans: {
+        ffmpeg: './ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+                mp4: true,
+                mp4Flags: '[movflags=frag_keyframe+empty_moov]'
+            }
+        ]
+    }
 }
 
 var nms = new NodeMediaServer(config)
